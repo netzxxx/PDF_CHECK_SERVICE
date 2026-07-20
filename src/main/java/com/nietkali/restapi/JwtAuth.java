0078226b44fecdf8045cdce31ab36cfe6dc2b764
netzxxx <nietkali.nurmanov@gmail.com>
2026-07-20 16:49:33 +0500

    Updateing JWT Access and Refresh token

--- src/main/java/com/nietkali/restapi/JwtAuth.java
+++ src/main/java/com/nietkali/restapi/JwtAuth.java
@@ -21,16 +21,15 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.security.Key;
 import java.util.Collections;
 import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
@@ -38,7 +37,7 @@ class SecurityConfig {
     public SecurityConfig(JwtFilter jwtFilter) { this.jwtFilter=jwtFilter;}
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/login", "/swagger-ui/**", "/v3/api-docs/**", "/api/download/**", "/").permitAll().anyRequest().authenticated()).sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/login", "/api/auth/refresh", "/swagger-ui/**", "/v3/api-docs/**", "/api/download/**", "/").permitAll().anyRequest().authenticated()).sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
     @Bean
@@ -50,13 +49,31 @@ class SecurityConfig {
 @Component
 class JwtUtil {
     private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-    private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 3;
-    public String generateToken(String username){
-        return  Jwts.builder().setSubject(username).setIssuedAt(new Date()).setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME)).signWith(SECRET_KEY).compact();
+    private static final long ACCESS_T_T = 1000 * 60 * 15;
+    private static final long REFRESH_T_T = 1000 * 60 * 60 * 24 * 7;
+    private final Map<String, String> refreshtokensStorage = new ConcurrentHashMap<>();
+    public String generateAccessToken(String username){
+        return  Jwts.builder().setSubject(username).setIssuedAt(new Date()).setExpiration(new Date(System.currentTimeMillis() + ACCESS_T_T)).signWith(SECRET_KEY).compact();
+    }
+    public String generateRefreshToken(String username) {
+        String refreshToken = Jwts.builder().setSubject(username).setIssuedAt(new Date()).setExpiration(new Date(System.currentTimeMillis() + REFRESH_T_T)).signWith(SECRET_KEY).compact();
+        refreshtokensStorage.put(refreshToken, username);
+        return refreshToken;
     }
     public String extractUsername(String token){
         return Jwts.parserBuilder().setSigningKey(SECRET_KEY).build().parseClaimsJws(token).getBody().getSubject();
 
+    }
+    public boolean validaterefreshToken(String token){
+        try {
+            Jwts.parserBuilder().setSigningKey(SECRET_KEY).build().parseClaimsJws(token);
+            return  refreshtokensStorage.containsKey(token);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    public void invalidateRefreshToken (String token) {
+        refreshtokensStorage.remove(token);
     }
 }
 @Component
@@ -71,7 +88,7 @@ class JwtFilter extends OncePerRequestFilter{
             try {
                 String username = jwtUtil.extractUsername(token);
                 if (username!=null&&SecurityContextHolder.getContext().getAuthentication()==null){
-                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, token, Collections.emptyList());
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());
                     SecurityContextHolder.getContext().setAuthentication(authenticationToken);
                 }
             } catch (Exception ignored) {}
@@ -84,11 +101,24 @@ class AuthController {
     private final JwtUtil jwtUtil;
     public AuthController(JwtUtil jwtUtil) {this.jwtUtil=jwtUtil;}
     @PostMapping("/login")
-    public String login(@RequestBody AuthRequest request){
+    public JwtResponse login(@RequestBody AuthRequest request){
         if ("admin".equals(request.getUsername()) && "admin".equals(request.getPassword())){
-            return jwtUtil.generateToken(request.getUsername());
+            String accessToken = jwtUtil.generateAccessToken(request.getUsername());
+            String refreshToken = jwtUtil.generateRefreshToken(request.getUsername());
+            return new JwtResponse(accessToken, refreshToken);
         }
         throw new RuntimeException("Invalid credetial");
+    }
+    @PostMapping("/refresh")
+    public JwtResponse refresh (@RequestBody RefreshRequest request){
+        String refreshToken = request.getRefreshToken();
+        if (refreshToken != null && jwtUtil.validaterefreshToken(refreshToken)){
+            String username = jwtUtil.extractUsername(refreshToken);
+            String newAccessToekn = jwtUtil.generateAccessToken(username);
+            String newReshtoken = jwtUtil.generateRefreshToken(username);
+            jwtUtil.invalidateRefreshToken(refreshToken);
+            return new JwtResponse(newAccessToekn, newReshtoken);
+        } throw new RuntimeException("Invalid or Expired Refresh Token");
     }
 }
 class AuthRequest{
@@ -103,6 +133,27 @@ class AuthRequest{
 
     public void setPassword(String password) {
         this.password = password;
+    }
+}
+class RefreshRequest{
+    private String refreshToken;
+    public String getRefreshToken() {return refreshToken;}
+    public void setRefreshToken(String refreshToken) {this.refreshToken = refreshToken;}
+}
+class JwtResponse {
+    private final String accessToken;
+    private final String refreshToken;
+    public JwtResponse(String accessToken, String refreshToken){
+        this.accessToken=accessToken;
+        this.refreshToken=refreshToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
     }
 }
 public class JwtAuth {
